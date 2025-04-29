e287bf204ec893548e44a6750c276b777f359e31
BarryLhm <BarryLhm@outlook.com>
2025-04-29 23:17:55 +0800

    update

---
+++ S/COMMON.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2025/04/28-2025/04/28
+# @Author : BarryLhm
+# @File : COMMON.py
+
+import os
+from pathlib import Path
+termux_root = Path('/data/data/com.termux/files')
+termux_home = termux_root / 'home'
+termux_prefix = termux_root / 'usr'
+termux_bin = termux_prefix / 'bin'
+termux_tmp = termux_prefix / 'tmp'
+
+if __name__ == "__main__":
+	exit("Segmentation Fault") #lol
---
+++ S/template.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2025/04/28-20//
+# @Author : BarryLhm
+# @File : template.py
+
+import COMMON
+
+if __name__ == "__main__":
+	exit()
