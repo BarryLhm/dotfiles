c13718f1970edd88ab31c4a0a801f236be20f0d7
BarryLhm <BarryLhm@outlook.com>
2025-12-13 00:00:38 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ if status is-interactive
     end
     set PATH "$HOME/.local/bin:$PATH"
 end
+set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
