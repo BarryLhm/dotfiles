4245ba6e88b253c86be284e68597b449ed549c96
BarryLhm <BarryLhm@outlook.com>
2026-09-13 15:01:15 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     if [ "$TERM" = linux ]
         set -gx LANG C
     end
-    set PATH "$HOME/.local/bin:/data/data/com.termux/files/home/.cargo/bin:$PATH:/usr/share/steam/compatibilitytools.d/proton-cachyos-native/protonfixes/files/bin:/usr/share/steam/compatibilitytools.d/proton-cachyos-native/files/bin"
+    set PATH "$HOME/.local/bin:$HOME/.cargo/bin:$PATH:/usr/share/steam/compatibilitytools.d/proton-cachyos-native/protonfixes/files/bin:/usr/share/steam/compatibilitytools.d/proton-cachyos-native/files/bin"
 end
 set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
